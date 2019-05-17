de03bac9d305477694caa068113efb3f97cddabd
ckasdf1405 <zhengcczd@163.com>
2019-05-17 23:22:48 +0800

    2019-5-17

--- src/com/zcc/nowcoder/ReplaceSpace.java
+++ src/com/zcc/nowcoder/ReplaceSpace.java
@@ -2,4 +2,14 @@ package com.zcc.nowcoder;
 
 public class ReplaceSpace {
 
+    public String replaceSpace(StringBuffer str){
+        if (str == null){return null;}
+        StringBuffer sb =new StringBuffer();
+        for (int i=0;i<str.length();i++){
+            if (str.charAt(i)==' '){
+                sb.append("%20");
+            }else sb.append(str.charAt(i));
+        }
+        return sb.toString();
+    }
 }
