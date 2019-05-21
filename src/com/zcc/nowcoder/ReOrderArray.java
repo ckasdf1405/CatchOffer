296ba646b86438316e1926eef023f0e4ab2e41a4
ckasdf1405 <zhengcczd@163.com>
2019-05-21 18:33:04 +0800

    2019-5-21 调整数组排序使奇数位于偶数前方

---
+++ src/com/zcc/nowcoder/ReOrderArray.java
@@ -0,0 +1,29 @@
+package com.zcc.nowcoder;
+
+/**
+ * 输入一个整数数组，实现一个函数来调整该数组中数字的顺序，
+ * 使得所有的奇数位于数组的前半部分，所有的偶数位于数组的后半部分，
+ * 并保证奇数和奇数，偶数和偶数之间的相对位置不变。
+ */
+public class ReOrderArray {
+    public static int[] reOrderArray(int [] array) {
+        int tmp;
+        for (int i =0;i <array.length-1;i++){
+            for (int j=0;j<array.length-1;j++) {
+                if (array[j] %2 ==0 && array[j+1] %2 ==1){
+                    tmp =array[j];
+                    array[j] =array[j+1];
+                    array[j+1] =tmp;
+                }
+            }
+        }
+        return array;
+    }
+
+
+    public static void main(String[] args) {
+        int[] a ={1,2,3,4,5,6,7};
+        System.out.println(reOrderArray(a));
+    }
+
+}
